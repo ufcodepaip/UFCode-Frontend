fc84f804b8309b62840466c2cbc50cad6ea96c7a
guinoro <guinoronha@live.com>
2022-05-29 17:15:46 -0300

    Resolvido bug que o usuário poderia enviar um bug sem que estivesse na posição que fornece uma questão

--- src/components/ChallengeCode/index.js
+++ src/components/ChallengeCode/index.js
@@ -12,25 +12,30 @@ function Challenges(props) {
 
     async function handleCodeSubmission(e) {
         e.preventDefault()
-        console.log(props.challengeId)
-
-        const jsonData = {
-            codeInput: code, 
-            student_id: studentId, 
-            problem_id: challenge.id, 
-            language_id: null
+        if(challenge.id == null){
+            console.log(challenge.id + "teste")
+            alert("não há questões para sua posição")
         }
-
-        console.log(jsonData)
-        await submission(jsonData).then( res => {
-            if(res.data.error ==! false)
-                alert("Sucesso!!!")
-            else
-                alert("Erro. Razão do erro: \n\n\n" + res.data.error)
-
-        }).catch(error => {
-            alert("Erro. Razão do erro: \n sintaxe do código incorreta \n" + error)
-        })
+        else{
+            const jsonData = {
+                codeInput: code, 
+                student_id: studentId, 
+                problem_id: challenge.id, 
+                language_id: null
+            }
+    
+            console.log(jsonData)
+            await submission(jsonData).then( res => {
+                if(res.data.error ==! false)
+                    alert("Sucesso!!!")
+                else
+                    alert("Erro. Razão do erro: \n\n\n" + res.data.error)
+    
+            }).catch(error => {
+                alert("Erro. Razão do erro: \n sintaxe do código incorreta \n" + error)
+            })
+        }
+        
     }
 
     const placeholder = "Ao ler a entrada, use sempre input(), sem nenhum valor dentro dos parênteses. Exemplo: int(input()) para ler um número e convertê-lo para inteiro.\nSeu programa não deve conter acentos. Caso contrário, o sistema irá apontar erro em sua solução.\nAs saídas do seu programa devem seguir o padrão exibido em 'Saída'\n\nDivirta-se, bom jogo!"
